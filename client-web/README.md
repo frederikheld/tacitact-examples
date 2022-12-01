2d18b4476d0651fac2e9079fbbbc3f25b786b3d1
Frederik Held <git@frederikheld.de>
2022-12-01 23:03:08 +0100

    Improves proxy in client-web

--- client-web/README.md
+++ client-web/README.md
@@ -1,8 +1,8 @@
 # Web Client
 
-Start one of the server implementations to have a backend for `tacitact` sync and to have the client files served!
+Start the backend server that is configured in `../config.json` to have a backend for `tacitact` sync and to have the client files served!
 
-Then start the server in the `client-web` directory that serves the web client with `$ npm run serve`.
+Then start the server in the `client-web` directory that serves the web client and proxies the request to the backend server with `$ npm run serve`.
 
 > Note: do not open the `index.html` directly from the file system as it would come with restrictrions in the browser. Also you wouldn't have no access to tacitact as it is proxied through the web client's server.
 
--- client-web/index.js
+++ client-web/index.js
@@ -3,6 +3,7 @@
 const fs = require('fs')
 const path = require('path')
 const express = require('express')
+const httpProxy = require('http-proxy')
 
 const CONFIG = require('../config.json')
 
@@ -10,36 +11,38 @@ const CONFIG = require('../config.json')
 // start webserver:
 
 const app = express()
-
-
-// proxy client files from server:
-// This allows to hard-code the tacitact.js path in the client
-// and still be able to change the server port in the global config.
-app.use('/tacitact/tacitact.js', async (req, res) => {
-    try {
-        const response = await fetch('http://localhost:' + CONFIG.server.port + '/tacitact/tacitact.js')
-
-        if (!response.ok) {
-            res.status(response.status).send('ERROR: Could not proxy request to server. Route does not exist.')
-        } else {
-            const payload = await response.text()
-
-            res.set('Content-Type', 'text/javascript')
-            res.send(payload)
-        }
-    } catch (error) {
-        res.status(500).send('ERROR: Could not proxy request to server. Server did not answer. Don\'t forget to start a server!')
-    }
+const proxy = httpProxy.createProxyServer({
+    target: {
+        host: 'localhost',
+        port: CONFIG.server.port
+    },
+    ws: true
+})
+app.use('/', (req, res, next) => {
+    console.log('request:', req.url)
+    next()
 })
 
 // serve web client:
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     const fileContents = fs.readFileSync(path.join(__dirname, 'index.html'))
     res.set('Content-Type', 'text/html')
     res.send(fileContents)
 })
 
+// proxy all other http get requests:
+app.get('/*', (req, res) => {
+    console.log('proxying http get request', req.url)
+    proxy.web(req, res, { })
+})
 
-app.listen(CONFIG.clientWeb.port, () => {
+// start express and retreive http server instance:
+const httpServer = app.listen(CONFIG.clientWeb.port, () => {
     console.log('Client is being served on port ' + CONFIG.clientWeb.port)
+})
+
+// proxy websockets:
+httpServer.on('upgrade', (req, res, head) => {
+    console.log('proxying websocket request', req.url)
+    proxy.ws(req, res, head)
 })
